7809d1b7facebef49438d066d18dd025bfa5bf47
Shun Yao <ysmiracle@hotmail.com>
2018-06-03 04:15:57 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -37,7 +37,19 @@ export class InFrontOfYou extends React.Component {
 }
 
 export class ButcherShop extends React.Component {
-  // your code here
+  render() {
+    return (
+      <div class="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          <li>Tenderloin</li>
+          <li>Short ribs</li>
+          <li>Beef shin</li>
+          <li>Ribeye</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 
